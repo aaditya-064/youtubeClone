476d238c0800e663329accd4d5e83c388cbf92d1
unknown <aadityastha123456@gmail.com>
2024-06-17 16:13:59 +0545

    added owl carousel

--- files/script.js
+++ files/script.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 7,
+    margin: 10,
+    autoWidth: true,
+    dragEndSpeed: true,
   });
+});
+
+var owl = $(".owl-carousel");
+owl.on("mousewheel", ".owl-stage", function (e) {
+  if (e.deltaY > 0) {
+    owl.trigger("next.owl");
+  } else {
+    owl.trigger("prev.owl");
+  }
+  e.preventDefault();
+});
